3a91cc0d1502a67e06430b7d5d5b1450dcb12599
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-08-04 09:21:47 +0800

    1

--- js/account.js
+++ js/account.js
@@ -1,5 +1,18 @@
 async function getSelfData(){
     const userid = "F111111111";
     var response = await fetch("https://localhost:7285/api/Account/" + userid);
-
+    var body = await response.json();
+    var table = $(`#accountList tbody`);
+    $.each(body,function(index,data){
+        table.append(`
+            <tr>
+                <td>${index+1}</td>
+                <td>${data.userid}</td>
+                <td>${data.bankname}</td>
+                <td>${data.bankcode}</td>
+                <td>${data.bankaccount}</td>
+                <td>${data.amount}</td>
+            </tr>
+        `);
+    });
 }
