fb78f23c29b8519445203f4f8214535ab6028049
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-08-09 14:07:32 +0800

    1

--- assets/js/loginT.js
+++ assets/js/loginT.js
@@ -1,6 +1,6 @@
 $(`#login`).on(`click`,async function (e){
     e.preventDefault();
-    var formdata = new FormData()[0];
+    var formdata = new FormData(formAuthentication);
     var response = await fetch(`https://2884-61-222-180-215.ngrok-free.app/api/Login`,{
         method:"Post",
         body:formdata
