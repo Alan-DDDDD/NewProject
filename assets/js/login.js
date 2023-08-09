455b590624e58c043b7f37a541fb2af30739d4e9
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-08-09 13:12:39 +0800

    logintest

---
+++ assets/js/login.js
@@ -0,0 +1,18 @@
+checkToken();
+async function checkToken(){
+    let token = localStorage.getItem(`loginToken`);
+    var data = "https://alan-ddddd.github.io/NewProject/html/auth-login-basic.html";
+    if(token){
+        var response = await fetch(`https://2884-61-222-180-215.ngrok-free.app/api/Login` + token);
+        var responseData = await response.json();
+        if(responseData.status == 0){
+            data = responseData.data.url;
+        }
+        else{
+            open(data);
+        }
+    }
+    else{
+        open(data);
+    }
+}
