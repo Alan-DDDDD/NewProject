bfac31a0ad0293fcfffe6472ad8ec601b33bd460
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-08-09 13:52:27 +0800

    q

---
+++ assets/js/loginT.js
@@ -0,0 +1,10 @@
+async function login(){
+    event.defaultPrevented();
+    var formdata = new FormData()[0];
+    var response = await fetch(`https://2884-61-222-180-215.ngrok-free.app/api/Login`,{
+        method:"Post",
+        body:formdata
+    });
+    var responseData = await response.json();
+    console.log(responseData);
+}
