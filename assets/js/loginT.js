cc38236d85a585cb4eaa1671d6f28928f6ac2391
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-08-10 11:28:53 +0800

    1

--- assets/js/loginT.js
+++ assets/js/loginT.js
@@ -15,4 +15,24 @@ $(`#login`).on(`click`,async function (e){
             open(url,"_self");
         }
     }
+    else{
+        if(responseData.status == 1){
+            var formdata = new FormData(formAuthentication);
+            formdata.append(`check`,confirm(responseData.msg));
+            var response = await fetch(`https://8643-61-222-180-215.ngrok-free.app/api/Login`,{
+                method:"Post",
+                body:formdata
+            });
+            var responseData = await response.json();
+            console.log(responseData);
+            if(responseData.status == 0){
+                localStorage.setItem(`loginToken`,responseData.data);
+                let url = localStorage.getItem(`backUrl`)
+                if(url){
+                    localStorage.removeItem(`backUrl`); 
+                    open(url,"_self");
+                }
+            }
+        }
+    }
 })
