e7e790fd7f31073e00f9d38e5e71fb81e711232e
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-08-11 09:48:54 +0800

    1

--- assets/js/loginT.js
+++ assets/js/loginT.js
@@ -38,19 +38,19 @@ $(`#login`).on(`click`,async function (e){
     }
 })
 
-function callAPI(fprmdata){
-    var response = await fetch(`https://f11c-61-222-180-215.ngrok-free.app/api/Login`,{
-                method:"Post",
-                body:formdata
-            });
-            var responseData = await response.json();
-            console.log(responseData);
-            if(responseData.status == 0){
-                localStorage.setItem(`loginToken`,responseData.data);
-                let url = localStorage.getItem(`backUrl`)
-                if(url){
-                    localStorage.removeItem(`backUrl`); 
-                    open(url,"_self");
-                }
-            }
-}
+// function callAPI(fprmdata){
+//     var response = await fetch(`https://f11c-61-222-180-215.ngrok-free.app/api/Login`,{
+//                 method:"Post",
+//                 body:formdata
+//             });
+//             var responseData = await response.json();
+//             console.log(responseData);
+//             if(responseData.status == 0){
+//                 localStorage.setItem(`loginToken`,responseData.data);
+//                 let url = localStorage.getItem(`backUrl`)
+//                 if(url){
+//                     localStorage.removeItem(`backUrl`); 
+//                     open(url,"_self");
+//                 }
+//             }
+// }
