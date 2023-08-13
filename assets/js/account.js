9fdfe87880c976c15a4f34d6681ed817d32793d6
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-08-14 00:09:57 +0800

    1

--- assets/js/account.js
+++ assets/js/account.js
@@ -49,7 +49,7 @@ async function getSelfData(){
 function　getQR(code,account){
   var money = prompt("請問輸入轉帳金額");
   console.log(Number(money));
-  if(money == 0 || "NaN"){
+  if(money == (0 || "NaN")){
     alert("x");
   }
   let url = "https://chart.googleapis.com/chart?cht=qr&chs=400x400&chl=TWQRP%3A%2F%2F"&code&"NTTransfer%2F158%2F02%2FV1%3FD6%3D"&account&"%26D5%3D"&code&"%26D1%3D"&money*100&"%26D10%3D901";
