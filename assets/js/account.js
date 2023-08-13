f7ae1d5a64a9ecea109abee14af5651ac8787290
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-08-14 00:15:37 +0800

    1

--- assets/js/account.js
+++ assets/js/account.js
@@ -52,7 +52,7 @@ function　getQR(code,account){
   if(money == (0 || "NaN")){
     alert("x");
   }
-  let url = "https://chart.googleapis.com/chart?cht=qr&chs=400x400&chl=TWQRP%3A%2F%2F"&code&"NTTransfer%2F158%2F02%2FV1%3FD6%3D"&account&"%26D5%3D"&code&"%26D1%3D"&money*100&"%26D10%3D901";
+  let url = `https://chart.googleapis.com/chart?cht=qr&chs=400x400&chl=TWQRP%3A%2F%2F${code}NTTransfer%2F158%2F02%2FV1%3FD6%3D${account}%26D5%3D${code}%26D1%3D${money*100}%26D10%3D901`;
   console.log(url);
   //open(url,"_block");
 }
