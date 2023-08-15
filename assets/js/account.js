afef1f24e7c2a66a886f02030969f785d1e6eac1
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-08-15 11:38:44 +0800

    1

--- assets/js/account.js
+++ assets/js/account.js
@@ -14,32 +14,6 @@ async function getSelfData(){
     var table = $(`#AccountList tbody`);
     if(body.status == 0){
         $.each(body.data,function(index,data){
-            // table.append(`
-            // <tr>
-            //   <td>${index+1}</td>
-            //   <td>${data.userid}</td>
-            //   <td>${data.bankname}</td>
-            //   <td>${data.bankcode}</td>
-            //   <td>${data.bankaccount}</td>
-            //   <td>${data.amount}</td>
-            //   <td>
-            //     <div class="btn-group" role="group" aria-label="Second group">
-            //         <button type="button" class="btn btn-outline-secondary">
-            //           <i class="tf-icons bx bx-coin bx-tada"></i>
-            //         </button>
-            //         <button type="button" class="btn btn-outline-secondary">
-            //           <i class="tf-icons bx bx-refresh bx-flip-vertical bx-spin"></i>
-            //         </button>
-            //         <button type="button" class="btn btn-outline-secondary">
-            //           <i class="tf-icons bx bx-comment-detail"></i>
-            //         </button>
-            //         <a href="https://chart.googleapis.com/chart?cht=qr&chs=400x400&chl=TWQRP%3A%2F%2F${data.bankcode}NTTransfer%2F158%2F02%2FV1%3FD6%3D0000699509320849%26D5%3D013%26D1%3D50000%26D10%3D901" target="_blank" class="btn btn-outline-secondary">
-            //           <img src="https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl=TWQRP%3A%2F%2F${data.bankcode}NTTransfer%2F158%2F02%2FV1%3FD6%3D0000699509320849%26D5%3D013%26D1%3D50000%26D10%3D901" with="100" heigh="100" alt="一張圖片">
-            //         </a>
-            //     </div>
-            //   </td>
-            // </tr>
-            // `);
             appendData(data);
         });
     }
--- assets/js/accountReport.js
+++ assets/js/accountReport.js
@@ -1,3 +1,41 @@
-function getData(){
-    
+getSelfData();
+async function getSelfData(){
+    //const userid = "F111111111";
+    const userid = "F128933062";
+    var response = await fetch(url + "/api/Account/", {
+        method: "get",
+        headers: new Headers({
+          "ngrok-skip-browser-warning": "69420",
+          "authorization":localStorage.getItem("loginToken")
+        }),
+      });
+    var body = await response.json();
+    console.log(body);
+    var table = $(`#AccountList tbody`);
+    if(body.status == 0){
+        $.each(body.data,function(index,data){
+            appendData(data);
+        });
+    }
+    else{
+      openLogin();
+    }
+}
+
+function appendData(data){
+    $(`#cash`).append(`<tr>
+                            <td class="align-middle">${data.accountName}</td>
+                            <td class="py-3">
+                              <p class="mb-0">${data.amount}</p>
+                            </td>
+                       </tr>
+    `);
+}
+
+function appendMonthData(data){
+
+}
+
+function appendYearData(data){
+
 }
